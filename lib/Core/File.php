5978bc84992ca1b015cf4a9cdf129dc3d91b3dc7
stefan vandermeulen <stefan@netdust.be>
2024-11-17 13:45:55 +0100

    typecast methods

--- lib/Core/File.php
+++ lib/Core/File.php
@@ -28,7 +28,7 @@ class File {
      * @param string $file       full path to main plugin file
      * @param array  $root       dir where to go and look
      */
-    public function __construct( $file = __FILE__, $root_path = '' ) {
+    public function __construct( string $file = __FILE__, string $root_path = '' ) {
         $this->file = $file;
         $this->root = $root_path;
     }
@@ -39,7 +39,7 @@ class File {
      * @param  array  $parts parts of the path
      * @return string        dir name
      */
-    public function build_dir_from_array( $parts = array() ) {
+    public function build_dir_from_array( array $parts = array() ): string {
 
         $dir = '';
 
@@ -57,7 +57,7 @@ class File {
      * @param  mixed  $file file structure
      * @return string       full file path
      */
-    public function resolve_file_path( $file = '' ) {
+    public function resolve_file_path( string|array $file = '' ): string {
 
         if ( is_array( $file ) ) {
             $filename = array_pop( $file );
@@ -73,7 +73,7 @@ class File {
      * Gets the plugin root dir absolute path
      * @return string path
      */
-    public function plugin_path() {
+    public function plugin_path(): string {
         return plugin_dir_path( $this->file );
     }
 
@@ -81,7 +81,7 @@ class File {
      * Gets the plugin root dir url
      * @return string url
      */
-    public function plugin_url() {
+    public function plugin_url(): string {
         return plugin_dir_url( $this->file );
     }
 
@@ -90,7 +90,7 @@ class File {
      * @param  mixed $file if it's an array, the dir structure will be built
      * @return string      file absolute path
      */
-    public function file_path( $file = '' ) {
+    public function file_path( string|array $file = '' ): string {
         return $this->plugin_path() . $this->resolve_file_path( $file );
     }
 
@@ -99,7 +99,7 @@ class File {
      * @param  mixed $file if it's an array, the dir structure will be built
      * @return string      file url
      */
-    public function file_url( $file = '' ) {
+    public function file_url( string|array $file = '' ): string {
         return $this->plugin_url() . $this->resolve_file_path( $file );
     }
 
@@ -108,7 +108,7 @@ class File {
      * @param  mixed $dir if it's an array, the dir structure will be built
      * @return string     dir absolute path
      */
-    public function dir_path( $dir = '' ) {
+    public function dir_path( string|array $dir = '' ): string  {
         return $this->plugin_path() . $this->build_dir_from_array( (array) $dir );
     }
 
@@ -117,7 +117,7 @@ class File {
      * @param  mixed $dir if it's an array, the dir structure will be built
      * @return string     dir url
      */
-    public function dir_url( $dir = '' ) {
+    public function dir_url( string|array $dir = '' ): string {
         return $this->plugin_url() . $this->build_dir_from_array( (array) $dir );
     }
 
@@ -127,8 +127,8 @@ class File {
      * @param  string $file file name
      * @return string       asset file url
      */
-    public function asset_url( $type = '', $file = '' ) {
-        return $this->file_url( $this->root+['assets',$type,$file] );
+    public function asset_url( string $type = '', string $file = '' ): string {
+        return $this->file_url( (array)$this->root+['assets',$type,$file] );
     }
 
     /**
@@ -137,8 +137,8 @@ class File {
      * @param  string $file file name
      * @return string       asset file path
      */
-    public function asset_path( $type = '', $file = '' ) {
-        return $this->file_path( $this->root+['assets',$type,$file] );
+    public function asset_path( string $type = '', string $file = '' ): string {
+        return $this->file_path( (array)$this->root+['assets',$type,$file] );
     }
 
     /**
@@ -146,8 +146,8 @@ class File {
      * @param  string $file file name
      * @return string       template file url
      */
-    public function template_url( $type = '', $file = '' ) {
-        return $this->file_url( $this->root+['templates',$type,$file] );
+    public function template_url( string $type = '', string $file = '' ): string {
+        return $this->file_url( (array)$this->root+['templates',$type,$file] );
     }
 
     /**
@@ -155,8 +155,8 @@ class File {
      * @param  string $file file name
      * @return string       template file path
      */
-    public function template_path( $type = '', $file = '' ) {
-        return $this->file_path( $this->root+['templates',$type,$file] );
+    public function template_path( string $type = '', string $file = '' ): string {
+        return $this->file_path( (array)$this->root+['templates',$type,$file] );
     }
 
 }
